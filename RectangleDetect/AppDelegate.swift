9ed09a4ff53d1cd044da5bb541134f242426b863
frcocoatst <friedrichhaeupl@friedrichs-mbp.fritz.box>
2017-01-30 16:27:09 +0100

    Initial Commit

---
+++ RectangleDetect/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RectangleDetect
+//
+//  Created by Friedrich HAEUPL on 30.01.17.
+//  Copyright © 2017 Friedrich HAEUPL. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RectangleDetect/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RectangleDetect
+//
+//  Created by Friedrich HAEUPL on 30.01.17.
+//  Copyright © 2017 Friedrich HAEUPL. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
